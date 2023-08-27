ebddf9fe013d62fb03e485b9185de660fc06aee3
MadhavBaghla <122570270+MadhavBaghla@users.noreply.github.com>
2023-08-27 13:05:20 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import calendar  # Core Python Module
+from datetime import datetime  # Core Python Module
+
+import plotly.graph_objects as go  # pip install plotly
+import streamlit as st  # pip install streamlit
+from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
+
+
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+# --------------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
+
+
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# --- NAVIGATION MENU ---
+selected = option_menu(
+    menu_title=None,
+    options=["Data Entry", "Data Visualization"],
+    icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
+)
+
+# --- INPUT & SAVE PERIODS ---
+if selected == "Data Entry":
+    st.header(f"Data Entry in {currency}")
+    with st.form("entry_form", clear_on_submit=True):
+        col1, col2 = st.columns(2)
+        col1.selectbox("Select Month:", months, key="month")
+        col2.selectbox("Select Year:", years, key="year")
+
+        "---"
+        with st.expander("Income"):
+            for income in incomes:
+                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
+        with st.expander("Expenses"):
+            for expense in expenses:
+                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+        with st.expander("Comment"):
+            comment = st.text_area("", placeholder="Enter a comment here ...")
+
+        "---"
+        submitted = st.form_submit_button("Save Data")
+        if submitted:
+            period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+            incomes = {income: st.session_state[income] for income in incomes}
+            expenses = {expense: st.session_state[expense] for expense in expenses}
+            st.success("Data saved!")
+
+           
